9070e0bac49f0dfaf07da8a4b1a3376772977ec1
Arthur Queiroz <arthurqueiroz25@gmail.com>
2024-09-03 00:19:38 -0300

    removing comment

--- src/mapExample.java
+++ src/mapExample.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 public class mapExample {
     public static void main (String args[]) {
-        //simpleMap();
+        simpleMap();
         linkedHashMapExample();
     }
 
